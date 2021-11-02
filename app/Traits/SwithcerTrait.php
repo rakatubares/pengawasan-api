c4a8c0053277b9dda5b9fc37c58136a42072cf89
rakatubares <rakatubares@gmail.com>
2021-11-02 19:49:38 +0700

    make general empty traits and routes

---
+++ app/Traits/SwithcerTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+
+trait SwitcherTrait
+{
+	private $models = [];
+
+	private $resources = [];
+
+	/**
+	 * Get model by doc type
+	 * 
+	 * @param string $doc_type
+	 * @return Model
+	 */
+	public function getModel($doc_type)
+	{
+		if (array_key_exists($doc_type, $this->models)) {
+			$model = $this->models[$doc_type];
+		} else {
+			$model = null;
+		}
+		
+		return $model;
+	}
+
+	/**
+	 * Get resource by type
+	 * 
+	 * @param string $resource_type
+	 * @return Resource
+	 */
+	public function getResource($resource_type)
+	{
+		if (array_key_exists($resource_type, $this->resources)) {
+			$resource = $this->resources[$resource_type];
+		} else {
+			$resource = null;
+		}
+		
+		return $resource;
+	}
+}
--- routes/api.php
+++ routes/api.php
@@ -25,63 +25,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-/**
- * API for SBP
- */
-Route::apiResource('sbp', SbpController::class);
-Route::get('/sbp/{sbp_id}/details', [SbpController::class, 'showDetails']);
-Route::put('/sbp/{sbp_id}/publish', [SbpController::class, 'publish']);
-
-/**
- * API for BA Segel
- */
-Route::apiResource('segel', SegelController::class);
-Route::get('/segel/{segel_id}/details', [SegelController::class, 'showDetails']);
-Route::put('/segel/{segel_id}/publish', [SegelController::class, 'publish']);
-
-/**
- * API for Details
- */
-Route::prefix('{doc_type}/{doc_id}')->group(function() {
-	// Sarkut
-	Route::prefix('/sarkut')->group(function() {
-		Route::get('/', [DetailSarkutController::class, 'show']);
-		Route::post('/', [DetailSarkutController::class, 'store']);
-		Route::delete('/', [DetailSarkutController::class, 'destroy']);
-	});
-
-	// Barang
-	Route::prefix('/barang')->group(function() {
-		Route::get('/', [DetailBarangController::class, 'show']);
-		Route::post('/', [DetailBarangController::class, 'store']);
-		Route::delete('/', [DetailBarangController::class, 'destroy']);
-
-		// Item barang
-		Route::prefix('item')->group(function() {
-			Route::get('/', [DetailBarangItemController::class, 'index']);
-			Route::post('/', [DetailBarangItemController::class, 'store']);
-			Route::get('/{item_id}', [DetailBarangItemController::class, 'show']);
-			Route::put('/{item_id}', [DetailBarangItemController::class, 'update']);
-			Route::delete('/{item_id}', [DetailBarangItemController::class, 'destroy']);
-		});
-	});
-
-	// Bangunan
-	Route::prefix('/bangunan')->group(function() {
-		Route::get('/', [DetailBangunanController::class, 'show']);
-		Route::post('/', [DetailBangunanController::class, 'store']);
-		Route::delete('/', [DetailBangunanController::class, 'destroy']);
-	});
-
-	// Badan
-	Route::prefix('/badan')->group(function() {
-		Route::get('/', [DetailBadanController::class, 'show']);
-		Route::post('/', [DetailBadanController::class, 'store']);
-		Route::delete('/', [DetailBadanController::class, 'destroy']);
-	});
-});
-
-
 Route::get('test', function() {
 	# code...
 })->middleware('permission');
