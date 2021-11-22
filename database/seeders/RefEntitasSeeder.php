90f51f726f183f6ca75a29d57f2a0b6628ec175e
rakatubares <rakatubares@gmail.com>
2021-11-22 09:11:23 +0700

    modify entity to only personal

--- database/seeders/RefEntitasSeeder.php
+++ database/seeders/RefEntitasSeeder.php
@@ -20,28 +20,28 @@ class RefEntitasSeeder extends Seeder
 		for ($i=0; $i < 100; $i++) { 
 			$jenis_entitas = $faker->randomElement(['perorangan', 'badan usaha']);
 
-			if ($jenis_entitas == 'perorangan') {
-				$gender = $faker->randomElement(['male', 'female']);
-				RefEntitas::create([
-					'jenis_entitas' => $jenis_entitas,
-					'nama' => $faker->name($gender),
-					'jenis_kelamin' => $gender,
-					'tanggal_lahir' => $faker->date(),
-					'warga_negara' => $faker->country(),
-					'jenis_identitas' => $faker->regexify('[A-Z]{3}'),
-					'nomor_identitas' => $faker->regexify('[0-9]{6,15}'),
-					'pekerjaan' => $faker->jobTitle(),
-					'alamat' => $faker->address()
-				]);
-			} else {
-				RefEntitas::create([
-					'jenis_entitas' => $jenis_entitas,
-					'nama' => $faker->company(),
-					'jenis_identitas' => $faker->regexify('[A-Z]{3}'),
-					'nomor_identitas' => $faker->regexify('[0-9]{6,15}'),
-					'alamat' => $faker->address()
-				]);
-			}
+			// if ($jenis_entitas == 'perorangan') {
+			$gender = $faker->randomElement(['male', 'female']);
+			RefEntitas::create([
+				'jenis_entitas' => 'perorangan',
+				'nama' => $faker->name($gender),
+				'jenis_kelamin' => $gender,
+				'tanggal_lahir' => $faker->date(),
+				'warga_negara' => $faker->country(),
+				'jenis_identitas' => $faker->regexify('[A-Z]{3}'),
+				'nomor_identitas' => $faker->regexify('[0-9]{6,15}'),
+				'pekerjaan' => $faker->jobTitle(),
+				'alamat' => $faker->address()
+			]);
+			// } else {
+			// 	RefEntitas::create([
+			// 		'jenis_entitas' => $jenis_entitas,
+			// 		'nama' => $faker->company(),
+			// 		'jenis_identitas' => $faker->regexify('[A-Z]{3}'),
+			// 		'nomor_identitas' => $faker->regexify('[0-9]{6,15}'),
+			// 		'alamat' => $faker->address()
+			// 	]);
+			// }
 			
 		}
 	}
