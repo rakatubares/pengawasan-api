4e62c640eb4c66e01e23d183be21b16d87d1fa49
rakatubares <rakatubares@gmail.com>
2021-12-16 14:07:13 +0700

    create riksa controller

---
+++ app/Http/Controllers/RiksaController.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\RiksaResource;
+use App\Http\Resources\RiksaTableResource;
+use App\Models\Riksa;
+use App\Traits\DokumenTrait;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class RiksaController extends Controller
+{
+	use DokumenTrait;
+
+	private $tipe_dok = 'BA';
+	private $agenda_dok = '/RIKSA/KPU.03/BD.05/';
+
+	/*
+	 |--------------------------------------------------------------------------
+	 | DISPLAY functions
+	 |--------------------------------------------------------------------------
+	 */
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$all_riksa = Riksa::orderBy('created_at', 'desc')
+			->orderBy('no_dok', 'desc')
+			->get();
+		$riksa_list = RiksaTableResource::collection($all_riksa);
+		return $riksa_list;
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		$riksa = new RiksaResource(Riksa::findOrFail($id));
+		return $riksa;
+	}
+
+	/*
+	 |--------------------------------------------------------------------------
+	 | Data modify functions
+	 |--------------------------------------------------------------------------
+	 */
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request, $linked_doc=false)
+	{
+		// Validate data penindakan if not from linked doc
+		if ($linked_doc == false) {
+			$this->validatePenindakan($request); 
+		}
+
+		DB::beginTransaction();
+		try {
+			// Save data pemeriksaan
+			$no_dok_lengkap = $this->tipe_dok . '-' . $this->agenda_dok;
+			$riksa = Riksa::create([
+				'agenda_dok' => $this->agenda_dok,
+				'no_dok_lengkap' => $no_dok_lengkap,
+				'kode_status' => 100,
+			]);
+
+			// Save data penindakan and create object relation
+			if ($linked_doc == false) {
+				$this->storePenindakan($request, 'riksa', $riksa->id);
+			}
+
+			// Commit store query
+			DB::commit();
+
+			// Return created document
+			$riksa_resource = new RiksaResource(Riksa::findOrFail($riksa->id));
+			return $riksa_resource;
+		} catch (\Throwable $th) {
+			DB::rollBack();
+			throw $th;
+		}
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		// Check if document is not published yet
+		$is_unpublished = $this->checkUnpublished(Riksa::class, $id);
+
+		// Update if not published
+		if ($is_unpublished) {
+			DB::beginTransaction();
+
+			try {
+				// Update data penindakan
+				$this->validatePenindakan($request); 
+				$this->updatePenindakan($request);
+
+				// Commit store query
+				DB::commit();
+
+				// Return updated SBP
+				$riksa_resource = new RiksaResource(Riksa::findOrFail($id));
+				$result = $riksa_resource;
+			} catch (\Throwable $th) {
+				DB::rollBack();
+				throw $th;
+			}
+		} else {
+			$result = response()->json(['error' => 'Dokumen sudah diterbitkan, tidak dapat mengupdate dokumen.'], 422);
+		}
+		
+		return $result;
+	}
+
+	/**
+	 * Terbitkan penomoran dokumen
+	 * 
+	 * @param  int  $id
+	 */
+	public function publish($id)
+	{
+		// Create array from SBP object
+		$riksa = new RiksaResource(Riksa::find($id));
+		$arr = json_decode($riksa->toJson(), true);
+
+		// Check penindakan date
+		$year = $this->datePenindakan(Riksa::class, $id);
+	
+		// Publish each document
+		foreach ($arr['dokumen'] as $type => $data) {
+			$this->publishDocument($type, $data['id'], $year);
+		}
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+}
---
+++ app/Http/Resources/RiksaResource.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RiksaResource extends JsonResource
+{
+	/**
+     * Create a new resource instance.
+     *
+     * @param  mixed  $resource
+     * @return void
+     */
+    public function __construct($resource, $element=null)
+    {
+        $this->resource = $resource;
+		$this->element = $element;
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        $riksa = [
+			'id' => $this->id,
+			'no_dok' => $this->no_dok,
+			'agenda_dok' => $this->agenda_dok,
+			'thn_dok' => $this->thn_dok,
+			'no_dok_lengkap' => $this->no_dok_lengkap,
+		];
+
+		$penindakan = new PenindakanResource($this->penindakan, 'basic');
+		$status = new RefStatusResource($this->status);
+		$objek = new ObjectResource($this->penindakan->objectable, $this->penindakan->object_type);
+		$dokumen = new PenindakanResource($this->penindakan, 'dokumen');
+
+		if ($this->element == 'basic') {
+			$array = $riksa;
+			$array['kode_status'] = $this->kode_status;
+		} else if ($this->element == 'objek') {
+			$array = $objek;
+		} else {
+			$array = [
+				'main' => [
+					'type' => 'riksa',
+					'data' => $riksa
+				],
+				'penindakan' => $penindakan,
+				'status' => $status,
+				'objek' => $objek,
+				'dokumen' => $dokumen,
+			];
+		}
+
+		return $array;
+    }
+}
