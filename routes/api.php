a3fb0461526c91756519b43530165fd3cacc1a4e
rakatubares <rakatubares@gmail.com>
2022-02-02 08:56:28 +0700

    add edit data function

--- routes/api.php
+++ routes/api.php
@@ -40,6 +40,7 @@ Route::get('/penindakan/{id}', [PenindakanController::class, 'show']);
  */
 Route::apiResource('reekspor', DokReeksporController::class);
 Route::get('/reekspor/{reekspor_id}/display', [DokReeksporController::class, 'display']);
+Route::put('/reekspor/{reekspor_id}/publish', [DokReeksporController::class, 'publish']);
 
 /**
  * API for Details
