ac6c3bcc27acab8748a363fab55ad1ae3b520c59
rakatubares <rakatubares@gmail.com>
2022-04-13 08:53:14 +0700

    FIX BUG delete sbpn if penindakan is deleted

--- app/Models/Penindakan.php
+++ app/Models/Penindakan.php
@@ -237,6 +237,9 @@ class Penindakan extends Model
 			if ($penindakan->sbp != null) {
 				$penindakan->sbp->delete();
 			}
+			if ($penindakan->sbpn != null) {
+				$penindakan->sbpn->delete();
+			}
 			if ($penindakan->segel != null) {
 				$penindakan->segel->delete();
 			}
