b22bb13e642dad50b7a10d12433320f2dae62496
rakatubares <rakatubares@gmail.com>
2022-07-06 14:59:23 +0700

    add entity search and edit, add search country features

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ use App\Http\Controllers\RefKategoriBarangController;
 use App\Http\Controllers\RefKemasanController;
 use App\Http\Controllers\RefKepercayaanSumberController;
 use App\Http\Controllers\RefLokasiController;
+use App\Http\Controllers\RefNegaraController;
 use App\Http\Controllers\RefSatuanController;
 use App\Http\Controllers\RefSprintController;
 use App\Http\Controllers\RefUserCacheController;
@@ -226,6 +227,12 @@ Route::post('satuan/search', [RefSatuanController::class, 'search']);
 Route::get('kategori', [RefKategoriBarangController::class, 'index']);
 Route::get('kategori/{id}', [RefKategoriBarangController::class, 'show']);
 Route::post('kategori/search', [RefKategoriBarangController::class, 'search']);
+
+/**
+ * API for Kategori Barang
+ */
+Route::get('negara/{kode}', [RefNegaraController::class, 'show']);
+Route::post('negara/search', [RefNegaraController::class, 'search']);
 
 /**
  * API for Bandara
